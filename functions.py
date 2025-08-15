10fb3cd0d627530af3ad66ad6c94a540a2e4cb9a
omar desouki <omar.desouki2@gmail.com>
2025-08-15 20:35:51 +0300

    explored the data and created multiple df

---
+++ functions.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import kagglehub
+import shutil
+import os
